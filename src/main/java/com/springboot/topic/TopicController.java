dfce516961c1734d575f29bf001012eaf280e811
kiran <kirankgpro@gmail.com>
2018-09-05 19:55:56 +0530

    initial commit

---
+++ src/main/java/com/springboot/topic/TopicController.java
@@ -0,0 +1,26 @@
+package com.springboot.topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@Autowired
+	private TopicService topicService;
+	
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics(){
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("topic/{id}")
+	public Topic getTopic(@PathVariable int id) {
+		return topicService.getTopic(id);
+	}
+	
+}
---
+++ src/main/java/com/springboot/topic/TopicService.java
@@ -0,0 +1,29 @@
+package com.springboot.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = Arrays.asList(
+			new Topic(1, "Spring"," Spring framework"),
+			new Topic(2, "java", "java lambda"),
+			new Topic(3, "javascript", "javascript advance"),
+			new Topic(4, "Html5", "Latest html5 concepts"));
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(int id) {
+		for(Topic topic : topics) {
+			if(topic.getId()==(id))
+				return topic;
+		}
+		return null;
+	}
+	
+}
